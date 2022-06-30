b643b08c6427d67a68b02955dc9c5c47f5ee5be5
Qbss <wuyang5207@outlook.com>
2022-06-30 23:40:37 +0800

    第三章

---
+++ 3,3/base.c
@@ -0,0 +1,15 @@
+//
+// Created by Qbss on 2022/6/30.
+//
+
+#include <stdio.h>
+
+int main(void) {
+
+    int x = 100;
+
+    printf("dec = %d; octal = %o; hex = %x\n",x,x,x);
+    printf("dec = %d; octal = %#o; hex = %#x\n",x,x,x);
+
+    return 0;
+}
